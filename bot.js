18d56bf1b337ae4b6626fa406292f2c7bdc04d1f
UPUSERDEV33 <54555776+UPUSERDEV33@users.noreply.github.com>
2019-08-29 09:14:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -314,7 +314,7 @@ function play(guild, song) {
 
 
 client.on('message', message => {
-    if (message.content === 'khelp2') {
+    if (message.content === 'khelp') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
         .setDescription('**برفكس البوت (k)**')
